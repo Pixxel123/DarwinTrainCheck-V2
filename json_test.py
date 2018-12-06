5466ac61485c29cfd884d37e84b95a7f445cd4b4
pixxel123 <hellhound123@live.co.uk>
2018-12-06 14:28:50 +0000

    Added parsing stuff

--- json_test.py
+++ json_test.py
@@ -11,11 +11,12 @@ try:
     with urlopen("https://huxley.apphb.com/all/" + dep_station + "/to/" + arr_station + "/" + str(time1) + "?accessToken=" + jsonToken) as response:
         source1 = response.read().decode('utf-8')
 
-    with urlopen("https://huxley.apphb.com/dep/ecr/to/whs?accessToken=" + jsonToken) as  response:
+    with urlopen("https://huxley.apphb.com/dep/ecr/to/whs?accessToken=" + jsonToken) as response:
         source2 = response.read().decode('utf-8')
-    data1 = json.loads(source1) # TTH -> ECR journey loaded as dict
-    data2 = json.loads(source2) # ECR -> WHS journey
-except urllib.error.URLError:
+    data1 = json.loads(source1)  # TTH -> ECR journey loaded as dict
+    data2 = json.loads(source2)  # ECR -> WHS journey
+except urllib.error.URLError as e:
+    ResponseData = e.reason
     print('No data connection')
 # WHS -> TTH rsid: SN324200
 # TODO: Use rsid value to return same journey information
@@ -24,18 +25,22 @@ except urllib.error.URLError:
 
 print('Departure Station: ' + str(data1.get('crs')))
 print('Arrival Station: ' + str(data1.get('filtercrs')))
+print('-' * 30)
 
 try:
-    for i in range(0,len(data1['trainServices'])):
+    for i in range(0, len(data1['trainServices'])):
         print('Service RSID: ' + str(data1['trainServices'][i]['rsid']))
         print('Scheduled arrival time: ' + str(data1['trainServices'][i]['sta']))
+        if data1['trainServices'][i]['sta'] == '15:03':
+            print(data1['trainServices'][i]['serviceID'])
+            print(data1['trainServices'][i]['adhocAlerts'])
+        print('Actual arrival: ' + str(data1['trainServices'][i]['eta']))
         if data1['trainServices'][i]['etd'] == 'On time':
-            print('\n')
+            print('')
         else:
             print('Status: ' + str(data1['trainServices'][i]['etd']))
-        print('Actual arrival time: ' + str(data1['trainServices'][i]['eta']))
         # print('Status: ' + str(data1['trainServices'][i]['etd']))
-        print(data1['trainServices'][i]['origin'])
+        # print(data1['trainServices'][i]['origin'])
 except TypeError:
     print('There is no train service data')
 try:
