7ef48c1394f09a76b87c33f532ba5253e649f620
Pixxel123 <36304206+Pixxel123@users.noreply.github.com>
2018-12-09 23:22:14 +0000

    Implementing lookback for cancelled services

--- json_test.py
+++ json_test.py
@@ -4,14 +4,14 @@ from darwin_token import DARWIN_KEY
 jsonToken = DARWIN_KEY
 
 train_station = {'work_station': 'whs', 'home_station': 'bal', 'connect_station': 'clj'}
-user_time = {'morning_time': ['0821', '0853', '2109'], 'evening_time': ['1703', '1733'], 'connect_time': ['0834', '0843']}
+user_time = {'morning_time': ['0821', '0853', '2309'], 'evening_time': ['1703', '1733'], 'connect_time': ['0834', '0843']}
 
 
 def darwinChecker(departure_station, arrival_station, user_time):
     response = requests.get("https://huxley.apphb.com/all/" + str(departure_station) + "/to/" + str(arrival_station) + "/" + str(user_time), params={"accessToken": jsonToken})
     response.raise_for_status()    # this makes an error if something failed
     data1 = response.json()
-
+    train_service = data1['trainServices']
     # WHS -> TTH rsid: SN324200
 
     print('Departure Station: ' + str(data1.get('crs')))
@@ -19,15 +19,18 @@ def darwinChecker(departure_station, arrival_station, user_time):
     print('-' * 30)
 
     try:
-        for train_service in data1['trainServices']:
+        for index, train_service in enumerate(train_service):
             if train_service['sta'].replace(':', '') in user_time:  # replaces sta time with values in user_time
                 print('Service ID: ' + str(train_service['serviceID']))
                 print('Service RSID: ' + str(train_service['rsid']))
                 print('Scheduled arrival time: ' + str(train_service['sta']))
-                print('Actual arrival: ' + str(train_service['eta']))
+                print('Scheduled departure time: ' + str(train_service['std']))
+                print('Status: ' + str(train_service['eta']))
                 print('*' * 20)
-                if train_service['etd'] == 'On time':
-                    print(train_service['serviceID'][train_service] - 1 + ' Testing previous')
+            else:
+                for index, service in enumerate(data1['trainServices']):
+                    if service['eta'] == '22:54':
+                        print(f'this one looks backwards {train_service[index - 1]["sta"]}')
             # else:
             #     print('Status: ' + str(train_service['etd'])
             # print('Status: ' + str(train_service['etd']))
