d27dd1df3457df72aa910d823b853d50b6707921
pixxel123 <hellhound123@live.co.uk>
2018-12-13 17:14:11 +0000

    Fixed count for services lookup and improved exception catching

--- json_test.py
+++ json_test.py
@@ -13,16 +13,14 @@ def darwinChecker(departure_station, arrival_station, user_time):
     response.raise_for_status()    # this makes an error if something failed
     data1 = response.json()
     train_service = data1["trainServices"]
-    # WHS -> TTH rsid: SN324200
-    # print(type(train_service))
     print('Departure Station: ' + str(data1.get('crs')))
     print('Arrival Station: ' + str(data1.get('filtercrs')))
     print('-' * 40)
     try:
-        found_service = 0
+        found_service = 0  # keeps track of services so note is generated if service not in user_time
         for index, service in enumerate(train_service):
             if service['sta'].replace(':', '') in user_time:  # replaces sta time with values in user_time
-                found_service += 1
+                found_service += 1  # increments for each service in user_time
                 print('Service RSID: ' + str(train_service[index]['rsid']))
                 print('Scheduled arrival time: ' + str(train_service[index]['sta']))
                 print('Scheduled departure time: ' + str(train_service[index]['std']))
@@ -32,24 +30,24 @@ def darwinChecker(departure_station, arrival_station, user_time):
                     # print('The ' + str(train_service[index]['sta']) + ' service is cancelled.')
                     print('Previous train departure time: ' + str(train_service[index - 1]['sta']))
                     print('Previous train status: ' + str(train_service[index - 1]['eta']))
-        if found_service == 0:
+        if found_service == 0:  # if no service is found
             print('The services currently available are not specified in user_time.')
     except TypeError:
         print('There is no train service data')
     try:
-        print('\nNRCC Messages: ' + str(data1['nrccMessages'][0]['value']))
+        # print('\nNRCC Messages: ' + str(data1['nrccMessages'][0]['value']))
         NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
         myline = NRCCRegex.search(data1['nrccMessages'][0]['value'])  # regex searches through nrccMessages
         print('\nNRCC Messages: ' + myline.group(1))  # prints parsed NRCC message
-    except TypeError:
+    except (TypeError, AttributeError) as error:  # tuple catches multiple errors, AttributeError for None value
         print('There is no NRCC data currently available\n')
 
 
 print('Morning Journey'.center(50, '='))
 darwinChecker(train_station['home_station'], train_station['connect_station'], user_time['morning_time'])
 
-# print('Connection Journey'.center(50, '='))
-# darwinChecker(train_station['connect_station'], train_station['work_station'], user_time['connect_time'])
+print('Connection Journey'.center(50, '='))
+darwinChecker(train_station['connect_station'], train_station['work_station'], user_time['connect_time'])
 
-# print('Evening Journey'.center(50, '='))
-# darwinChecker(train_station['work_station'], train_station['home_station'], user_time['evening_time'])
+print('Evening Journey'.center(50, '='))
+darwinChecker(train_station['work_station'], train_station['home_station'], user_time['evening_time'])
