0f9dcadd67fcc57eacb55c05c0ad99ee0671b70a
pixxel123 <hellhound123@live.co.uk>
2018-12-13 12:52:03 +0000

    Added count for services lookup, prints message if services are not in user_time

--- json_test.py
+++ json_test.py
@@ -18,33 +18,29 @@ def darwinChecker(departure_station, arrival_station, user_time):
     print('Departure Station: ' + str(data1.get('crs')))
     print('Arrival Station: ' + str(data1.get('filtercrs')))
     print('-' * 40)
-    # found_service = 0
+    found_service = 0
+    if found_service == 0:
+            print('The services currently available are not specified in user_time.')
     try:
         for index, service in enumerate(train_service):
             if service['sta'].replace(':', '') in user_time:  # replaces sta time with values in user_time
+                found_service += 1
                 print('Service ID: ' + str(train_service[index]['serviceID']))
                 print('Service RSID: ' + str(train_service[index]['rsid']))
                 print('Scheduled arrival time: ' + str(train_service[index]['sta']))
                 print('Scheduled departure time: ' + str(train_service[index]['std']))
                 print('Status: ' + str(train_service[index]['eta']))
                 print('-' * 40)
-                # found_service += 1
-                # print(found_service)
-                # return found_service
+                print(found_service)
                 if service['eta'] == 'Cancelled':
                     # print('The ' + str(train_service[index]['sta']) + ' service is cancelled.')
                     print('Previous train departure time: ' + str(train_service[index - 1]['sta']))
                     print('Previous train status: ' + str(train_service[index - 1]['eta']))
-            else:
-            	print('Service not in user_time')
-            	break
-        # if found_service != 1:
-        #     print('Service check test: ' + found_service)
-        #     print('The services currently available are not specified in user_time.')
+        return found_service
     except TypeError:
         print('There is no train service data')
     try:
-        # print('\nNRCC Messages: ' + str(data1['nrccMessages'][0]['value']))
+        print('\nNRCC Messages: ' + str(data1['nrccMessages'][0]['value']))
         NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
         myline = NRCCRegex.search(data1['nrccMessages'][0]['value'])  # regex searches through nrccMessages
         print('\nNRCC Messages: ' + myline.group(1))  # prints parsed NRCC message
@@ -55,8 +51,8 @@ def darwinChecker(departure_station, arrival_station, user_time):
 print('Morning Journey'.center(50, '='))
 darwinChecker(train_station['home_station'], train_station['connect_station'], user_time['morning_time'])
 
-print('Connection Journey'.center(50, '='))
-darwinChecker(train_station['connect_station'], train_station['work_station'], user_time['connect_time'])
+# print('Connection Journey'.center(50, '='))
+# darwinChecker(train_station['connect_station'], train_station['work_station'], user_time['connect_time'])
 
-print('Evening Journey'.center(50, '='))
-darwinChecker(train_station['work_station'], train_station['home_station'], user_time['evening_time'])
+# print('Evening Journey'.center(50, '='))
+# darwinChecker(train_station['work_station'], train_station['home_station'], user_time['evening_time'])
