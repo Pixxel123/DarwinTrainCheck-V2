02a47cd687e7267136787173755117098085f238
pixxel123 <hellhound123@live.co.uk>
2019-03-08 16:36:55 +0000

    Cleaned up test functions

--- darwinapp.py
+++ darwinapp.py
@@ -106,7 +106,7 @@ def darwin_checker_dict(departure_station, arrival_station, user_time):
 
 @app.route('/darwincheck')
 def darwin_dict():
-    darwin_checker_dict(train_station['home_station'], train_station['connect_station'], user_time['morning_time'])
+    darwin_checker_dict(train_station['work_station'], train_station['connect_station'], user_time['evening_time'])
     return render_template('index.html', traindata=mytrains)
 
 
