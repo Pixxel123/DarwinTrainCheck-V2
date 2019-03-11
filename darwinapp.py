c8770ac99e2b0493494cbedb1717ad9f4e47b533
pixxel123 <hellhound123@live.co.uk>
2019-03-11 10:14:00 +0000

    Added evening journey leg and time

--- darwinapp.py
+++ darwinapp.py
@@ -17,7 +17,7 @@ print('##########################################')
 # jsonToken = DARWIN_KEY
 
 train_station = {'work_station': 'whs', 'home_station': 'orp', 'connect_station': 'lbg'}
-user_time = {'morning_time': ['0715', '0720', '1517'], 'evening_time': ['1723'], 'connect_time': ['0817', '0825']}
+user_time = {'morning_time': ['0715', '0720', '1517'], 'evening_time': ['1103', '1723'], 'connect_time': ['0817', '0825']}
 
 mytrains = {}
 # def url_request(departure_station, arrival_station, user_time):
