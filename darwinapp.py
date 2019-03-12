b4155ed4081586e293c0e72ccb24f0e4f2f8858a
pixxel123 <hellhound123@live.co.uk>
2019-03-12 09:53:33 +0000

    wip

--- darwinapp.py
+++ darwinapp.py
@@ -35,66 +35,32 @@ def hello():
     return text
 
 
-def darwinChecker(departure_station, arrival_station, user_time):
-    response = requests.get("https://huxley.apphb.com/all/" + str(departure_station) + "/to/" + str(arrival_station) + "/" + str(user_time), params={"accessToken": jsonToken})
-    response.raise_for_status()    # this makes an error if something failed
-    data1 = response.json()
-    train_service = data1["trainServices"]
-    print('Departure Station: ' + str(data1['crs']))
-    print('Arrival Station: ' + str(data1['filtercrs']))
-    print('-' * 40)
-    try:
-        found_service = 0  # keeps track of services so note is generated if service not in user_time
-        for index, service in enumerate(train_service):  # enumerate adds index value to train_service list
-            if service['sta'].replace(':', '') in user_time:  # replaces sta time with values in user_time
-                found_service += 1  # increments for each service in user_time
-                print('Service RSID: ' + str(train_service[index]['rsid']))
-                print('Scheduled arrival time: ' + str(train_service[index]['sta']))
-                print('Scheduled departure time: ' + str(train_service[index]['std']))
-                print('Status: ' + str(train_service[index]['eta']))
-                print('-' * 40)
-                if service['eta'] == 'Cancelled':
-                    print('The ' + str(train_service[index]['sta']) + ' service is cancelled.')
-                    print('Previous train departure time: ' + str(train_service[index - 1]['sta']))
-                    print('Previous train status: ' + str(train_service[index - 1]['eta']))
-        if found_service == 0:  # if no service is found
-            print('The services currently available are not specified in user_time.')
-    except TypeError:
-        print('There is no train service data')
-    try:
-        NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
-        myline = NRCCRegex.search(data1['nrccMessages'][0]['value'])  # regex searches through nrccMessages
-        print('\nNRCC Messages: ' + myline.group(1) + '\n')  # prints parsed NRCC message
-    except (TypeError, AttributeError) as error:  # tuple catches multiple errors, AttributeError for None value
-        print('\nThere is no NRCC data currently available\n')
-
-
 def darwin_checker_dict(departure_station, arrival_station, user_time):
     response = requests.get("https://huxley.apphb.com/all/" + str(departure_station) + "/to/" + str(arrival_station) + "/" + str(user_time) + '?', params={"accessToken": SECRET_KEY})
     response.raise_for_status()  # this makes an error if something failed
     data1 = response.json()
     mytrains['departure'] = str(data1['crs'])
     mytrains['arrival'] = str(data1['filtercrs'])
-    # try:
-    found_service = 0
-    for index, service in enumerate(data1['trainServices']):
-        if service['sta'].replace(':', '') in user_time:
-            found_service += 1
-            mytrains['rsid'] = str(data1['trainServices'][index]['rsid'])
-            mytrains['arrival_time'] = str(data1['trainServices'][index]['sta'])
-            mytrains['estimated_arrival'] = str(data1['trainServices'][index]['eta'])
-            if mytrains['estimated_arrival'] == 'On time':
-                mytrains['status'] = 'On time'
-            if mytrains['estimated_arrival'] != 'On time':
-                mytrains['status'] = 'Delayed'
-            if service['eta'] == 'Cancelled':
-                mytrains['status'] = 'Cancelled'
-                mytrains['alternate_service'] = str(data1['trainServices'][index - 1]['sta'])
-                mytrains['alternate_status'] = str(data1['trainServices'][index - 1]['eta'])
-    # if found_service == 0:  # if no service is found
-        # mytrains['state'] = 'The services currently available are not specified in user_time.'
-# except TypeError:
-    # mytrains['errorMessage'] = 'There is no train service data'
+    try:
+        found_service = 0
+        for index, service in enumerate(data1['trainServices']):
+            if service['sta'].replace(':', '') in user_time:
+                found_service += 1
+                mytrains['rsid'] = str(data1['trainServices'][index]['rsid'])
+                mytrains['arrival_time'] = str(data1['trainServices'][index]['sta'])
+                mytrains['estimated_arrival'] = str(data1['trainServices'][index]['eta'])
+                if mytrains['estimated_arrival'] == 'On time':
+                    mytrains['status'] = 'On time'
+                if mytrains['estimated_arrival'] != 'On time':
+                    mytrains['status'] = 'Delayed'
+                if service['eta'] == 'Cancelled':
+                    mytrains['status'] = 'Cancelled'
+                    mytrains['alternate_service'] = str(data1['trainServices'][index - 1]['sta'])
+                    mytrains['alternate_status'] = str(data1['trainServices'][index - 1]['eta'])
+            if found_service == 0:  # if no service is found
+                mytrains['state'] = 'The services currently available are not specified in user_time.'
+    except (TypeError, AttributeError) as error:
+        mytrains['errorMessage'] = 'There is no train service data'
     try:
         NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
         myline = NRCCRegex.search(data1['nrccMessages'][0]['value'])  # regex searches through nrccMessages
@@ -118,7 +84,7 @@ def darwin_connect():
 
 @app.route('/evening')
 def darwin_evening():
-    darwin_checker_dict(train_station['home_station'], train_station['connect_station'], user_time['evening_time'])
+    darwin_checker_dict(train_station['work_station'], train_station['connect_station'], user_time['evening_time'])
     return render_template('index.html', traindata=mytrains)
 
 
