9fc14dc46a044383b6e14245b3c8485ba0c6f31b
Pixxel123 <36304206+Pixxel123@users.noreply.github.com>
2019-03-31 16:44:22 +0100

    User can pass train requests to URL

--- darwinapp.py
+++ darwinapp.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 # from flask import jsonify
 # import json
 import requests
@@ -8,28 +9,29 @@ import os
 from types import SimpleNamespace
 app = Flask(__name__)
 
-print('##########################################')
+# print('##########################################')
 # print(SECRET_KEY)
-print(os.environ)
-print(os.environ.get('DARWIN_KEY', 'testing'))
-# SECRET_KEY = os.environ.get('DARWIN_KEY', None)
-SECRET_KEY = '4245c8a6-8a88-4727-9f99-29875e6914b4'
-print(SECRET_KEY)
-print('##########################################')
+# print(os.environ)
+# print(os.environ.get('DARWIN_KEY', 'testing'))
+SECRET_KEY = os.environ.get('DARWIN_KEY', None)
+# print(SECRET_KEY)
+# print('##########################################')
 # jsonToken = DARWIN_KEY
-
-train_station = {'work_station': 'ecr', 'home_station': 'orp', 'connect_station': 'vic'}
-user_time = {'morning_time': ['1052'], 'evening_time': ['2342', '2345'], 'connect_time': ['0817', '0825']}
 
 mytrains = {}
 
-time_trains = {}
+# time_trains = {}
 
-time_of_day = 0
+# time_of_day = 0
 
 train_service_data = {}
 
 train_station_data = {}
+
+dest = 0
+origin = 0
+mytimes = 0
+myurl = 0
 
 
 @app.route("/")
@@ -39,65 +41,29 @@ def hello():
     return text
 
 
-# def darwin_checker(departure_station, arrival_station, query_time):
-#     global mytrains  # modifies the global copy of mytrains otherwise a new variable is created
-#     formatted_times = ",".join(query_time)
-#     response = requests.get("https://huxley.apphb.com/all/" + str(departure_station) + "/to/" + str(arrival_station) + "/" + str(formatted_times), params={"accessToken": SECRET_KEY})
-#     response.raise_for_status()  # this makes an error if something failed
-#     data1 = response.json()
-#     mytrains['departure'] = str(data1['crs'])
-#     mytrains['arrival'] = str(data1['filtercrs'])
-#     try:
-#         found_service = 0
-#         for index, service in enumerate(data1['trainServices']):  # indexes data for pulling of previous values
-#             if service['std'].replace(':', '') in formatted_times:
-#                 found_service += 1
-#                 new = {}  # new catches duplicate data before passing to mytrains
-#                 ignore_keys = ['estimated_arrival']
-#                 {k: v for k, v in new.items() if k not in ignore_keys}
-#                 new['serviceID'] = str(data1['trainServices'][index]['serviceID'])
-#                 new['arrival_time'] = str(data1['trainServices'][index]['std'])
-#                 new['estimated_arrival'] = str(data1['trainServices'][index]['etd'])
-#                 if new['estimated_arrival'] == 'On time':
-#                     new['status'] = 'On time'
-#                 if new['estimated_arrival'] != 'On time':
-#                     new['status'] = 'Delayed'
-#                 if new['estimated_arrival'] == 'Cancelled':
-#                     new['status'] = 'Cancelled'
-#                     new['alternate_service'] = str(data1['trainServices'][index - 1]['std'])
-#                     new['alternate_status'] = str(data1['trainServices'][index - 1]['etd'])
-#                 if all([mytrains[i] != new for i in mytrains]):  # checks if duplicates are in new
-#                     mytrains[index] = new
-#                     mytrains[index] = {}
-#                     mytrains[index]['serviceID'] = str(data1['trainServices'][index]['serviceID'])
-#                     mytrains[index]['arrival_time'] = str(data1['trainServices'][index]['std'])
-#                     mytrains[index]['estimated_arrival'] = str(data1['trainServices'][index]['etd'])
-#                     if mytrains[index]['estimated_arrival'] == 'On time':
-#                         mytrains[index]['status'] = 'On time'
-#                     if mytrains[index]['estimated_arrival'] != 'On time':
-#                         mytrains[index]['status'] = 'Delayed'
-#                     if mytrains[index]['estimated_arrival'] == 'Cancelled':
-#                         mytrains[index]['status'] = 'Cancelled'
-#                         mytrains[index]['alternate_service'] = str(data1['trainServices'][index - 1]['std'])
-#                         mytrains[index]['alternate_status'] = str(data1['trainServices'][index - 1]['etd'])
-#             elif found_service == 0:  # if no service is found
-#                 mytrains['state'] = 'The services currently available are not specified in user_time.'
-#     except (TypeError, AttributeError) as error:
-#         mytrains['errorMessage'] = 'There is no train service data'
-#     try:
-#         NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
-#         myline = NRCCRegex.search(data1['nrccMessages'][0]['value'])  # regex searches through nrccMessages
-#         mytrains['nrcc'] = myline.group(1)  # prints parsed NRCC message
-#     except (TypeError, AttributeError) as error:  # tuple catches multiple errors, AttributeError for None value
-#         mytrains['nrcc'] = 'No NRCC'
-#     return mytrains
+@app.route("/nametest", methods=["GET"])
+def web_test():
+    name = request.args.get('name')
+    colours = request.args.get('colours')
+    mytext = f"Hello, {name}. Your fave colour is {colours}."
+    return mytext
 
 
+@app.route("/gettrains", methods=["GET"])
+def trains_test():
+    origin = request.args.get('origin')
+    dest = request.args.get('dest')
+    mytimes = request.args.get('mytimes')
+    myurl = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}?accessToken=" + SECRET_KEY
+    return myurl
 
-def darwin_checker(departure_station, arrival_station, query_time):
+
+def checker():
     global mytrains  # modifies the global copy of mytrains otherwise a new variable is created
-    formatted_times = ",".join(query_time)
-    response = requests.get("https://huxley.apphb.com/all/" + str(departure_station) + "/to/" + str(arrival_station) + "/" + str(formatted_times), params={"accessToken": SECRET_KEY})
+    global myurl
+    global mytimes
+    # myurl = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}"
+    response = requests.get(myurl, params={"accessToken": SECRET_KEY})
     response.raise_for_status()  # this makes an error if something failed
     data1 = response.json()
     mytrains['departure'] = str(data1['crs'])
@@ -105,7 +71,7 @@ def darwin_checker(departure_station, arrival_station, query_time):
     try:
         found_service = 0
         for index, service in enumerate(data1['trainServices']):  # indexes data for pulling of previous values
-            if service['std'].replace(':', '') in formatted_times:
+            if service['std'].replace(':', '') in mytimes:
                 found_service += 1
                 train = SimpleNamespace(
                     serviceID=str(service['serviceID']),
@@ -138,39 +104,30 @@ def darwin_checker(departure_station, arrival_station, query_time):
     return mytrains
 
 
-def darwin_time(time_of_day):
-    global time_trains
-    # global sorted_trains
-    if time_of_day == 'morning_time':
-        time_trains = darwin_checker(train_station['home_station'], train_station['connect_station'], user_time[time_of_day])
-    if time_of_day == 'evening_time':
-        time_trains = darwin_checker(train_station['work_station'], train_station['connect_station'], user_time[time_of_day])
-    # sorted_trains = [train for i, train in time_trains.items() if isinstance(train, dict)]
-    return time_trains
-
-
 def time_trains_services():  # splits data into train services lookup
     global train_service_data
-    train_service_data = [j for i, j in time_trains.items() if isinstance(j, dict)]  # grabs train service data into dict
+    train_service_data = [j for i, j in mytrains.items() if isinstance(j, dict)]  # grabs train service data into dict
     return train_service_data
 
 
 def time_trains_location():  # splits data into crs, filtercrs and nrcc queries
     global train_station_data
-    train_station_data = {i: j for i, j in time_trains.items() if not isinstance(j, dict)}  # grabs [0] data into separate dict
+    train_station_data = {i: j for i, j in mytrains.items() if not isinstance(j, dict)}  # grabs [0] data into separate dict
     return train_station_data
 
 
-@app.route('/morning')
-def morning_page():
-    darwin_time('morning_time')
-    return render_template('index.html', traindata=darwin_time('morning_time'), trainstation=time_trains_location(), trainservices=time_trains_services())
-
-
-@app.route('/evening')
-def evening_page():
-    darwin_time('evening_time')
-    return render_template('index.html', traindata=darwin_time('evening_time'), trainstation=time_trains_location(), trainservices=time_trains_services())
+@app.route("/getstatus", methods=["GET"])
+def status_check():
+    global myurl
+    global mytimes
+    global origin
+    global dest
+    origin = request.args.get('origin')
+    dest = request.args.get('dest')
+    mytimes = request.args.get('mytimes')
+    myurl = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}"
+    output = checker()
+    return render_template('index.html', traindata=mytrains, trainstation=time_trains_location(), trainservices=time_trains_services())
 
 
 if __name__ == '__main__':
