4c18e1f6ee7b86a66c912a30854f5ce3df67e019
Pixxel123 <36304206+Pixxel123@users.noreply.github.com>
2019-04-09 20:36:25 +0100

    New version of darwintrain that does not use global variables and new template

--- darwinapp.py
+++ darwinapp.py
@@ -1,133 +1,83 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask import jsonify
-# import json
 import requests
 import re
 import os
 from types import SimpleNamespace
+
 app = Flask(__name__)
 
-# print('##########################################')
-# print(SECRET_KEY)
-# print(os.environ)
-# print(os.environ.get('DARWIN_KEY', 'testing'))
 SECRET_KEY = os.environ.get('DARWIN_KEY', None)
-# print(SECRET_KEY)
-# print('##########################################')
-# jsonToken = DARWIN_KEY
 
-mytrains = {}
-
-# time_trains = {}
-
-# time_of_day = 0
-
-train_service_data = {}
-
-train_station_data = {}
-
-dest = 0
-origin = 0
-mytimes = 0
-myurl = 0
+# test page to show that Flask is up and running
 
 
-@app.route("/")
-@app.route("/home")
-def hello():
-    text = 'Flask is up and running!'
-    return text
+@app.route('/')
+@app.route('/home')
+def home_page():
+    string = 'Hello World! Flask is running!'
+    return string
 
 
-@app.route("/nametest", methods=["GET"])
-def web_test():
-    name = request.args.get('name')
-    colours = request.args.get('colours')
-    mytext = f"Hello, {name}. Your fave colour is {colours}."
-    return mytext
+def url_parameters():  # defines lookup parameters in URL
+    origin = request.args.get('origin')  # origin station
+    dest = request.args.get('dest')  # destination station
+    mytimes = request.args.get('mytimes')  # times to lookup
+    check_url = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}"  # format URL for API lookup
+    response = requests.get(check_url, params={"accessToken": SECRET_KEY})  # get URL and make request, with API key
+    response.raise_for_status()  # raises the relevant error if status code is not 200
+    full_data = response.json()  # url data as a json is given name 'full_data'
+    return full_data, mytimes  # output tuple, url_parameters()[0] = full_data, url_parameters()[1] = mytimes
 
 
-@app.route("/gettrains", methods=["GET"])
-def trains_test():
-    origin = request.args.get('origin')
-    dest = request.args.get('dest')
-    mytimes = request.args.get('mytimes')
-    myurl = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}?accessToken=" + SECRET_KEY
-    return myurl
+def get_location():  # grabs stations data and NRCC messages
+    trains_location = url_parameters()  # assigns url to variable
+    for full_data in url_parameters()[0]:  # loops through full_data from url_parameters()
+        trains_location = {}  # empty dict is initialised
+        trains_location['departure'] = url_parameters()[0]['crs']
+        trains_location['arrival'] = url_parameters()[0]['filtercrs']
+        trains_location['generated'] = url_parameters()[0]['generatedAt']
+        try:
+            NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
+            myline = NRCCRegex.search(url_parameters()[0]['nrccMessages'][0]['value'])  # regex searches through nrccMessages
+            trains_location['nrcc'] = myline.group(1)  # prints parsed NRCC message
+        except (TypeError, AttributeError) as error:  # tuple catches multiple errors, AttributeError for None value
+            trains_location['nrcc'] = 'No NRCC'
+    return trains_location
 
 
-def checker():
-    global mytrains  # modifies the global copy of mytrains otherwise a new variable is created
-    global myurl
-    global mytimes
-    # myurl = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}"
-    response = requests.get(myurl, params={"accessToken": SECRET_KEY})
-    response.raise_for_status()  # this makes an error if something failed
-    data1 = response.json()
-    mytrains['departure'] = str(data1['crs'])
-    mytrains['arrival'] = str(data1['filtercrs'])
-    try:
-        found_service = 0
-        for index, service in enumerate(data1['trainServices']):  # indexes data for pulling of previous values
-            if service['std'].replace(':', '') in mytimes:
-                found_service += 1
-                train = SimpleNamespace(
-                    serviceID=str(service['serviceID']),
-                    arrival_time=str(service['std']),
-                    estimated_arrival=str(service['etd']),
-                    status='On time'
-                )
-                prior_service = data1['trainServices'][index - 1]
-                if train.estimated_arrival == 'Cancelled':
-                    train.status = 'Cancelled'
-                    train.alternate_service = str(prior_service['std'])
-                    train.alternate_status = str(prior_service['etd'])
-                elif train.estimated_arrival != 'On time':
-                    train.status = 'Delayed'
-                write_index = index
-                for i, v in mytrains.items():
-                    if isinstance(v, dict) and v['arrival_time'] == train.arrival_time:
-                        write_index = i
-                mytrains[write_index] = train.__dict__
-            elif found_service == 0:  # if no service is found
-                mytrains['state'] = 'The services currently available are not specified in user_time.'
-    except (TypeError, AttributeError) as error:
-        mytrains['errorMessage'] = 'There is no train service data'
-    try:
-        NRCCRegex = re.compile('^(.*?)[\.!\?](?:\s|$)')  # regex pulls all characters until hitting a . or ! or ?
-        myline = NRCCRegex.search(data1['nrccMessages'][0]['value'])  # regex searches through nrccMessages
-        mytrains['nrcc'] = myline.group(1)  # prints parsed NRCC message
-    except (TypeError, AttributeError) as error:  # tuple catches multiple errors, AttributeError for None value
-        mytrains['nrcc'] = 'No NRCC'
-    return mytrains
-
-
-def time_trains_services():  # splits data into train services lookup
-    global train_service_data
-    train_service_data = [j for i, j in mytrains.items() if isinstance(j, dict)]  # grabs train service data into dict
-    return train_service_data
-
-
-def time_trains_location():  # splits data into crs, filtercrs and nrcc queries
-    global train_station_data
-    train_station_data = {i: j for i, j in mytrains.items() if not isinstance(j, dict)}  # grabs [0] data into separate dict
-    return train_station_data
+def get_services():
+    train_services = url_parameters()  # name the returned tuple, train_services[0] = full_data, train_services[1] = mytimes
+    found_service = 0
+    service_times = {}
+    for index, service in enumerate(train_services[0]['trainServices']):
+        service_info = train_services[1]
+        if service['std'].replace(':', '') in service_info:
+            found_service += 1
+            train = SimpleNamespace(
+                serviceID=str(service['serviceID']),
+                arrival_time=str(service['std']),
+                estimated_arrival=str(service['etd']),
+                status='On time')
+            prior_service = train_services[0]['trainServices'][index - 1]
+            if train.estimated_arrival == 'Cancelled':
+                train.status = 'Cancelled'
+                train.alternate_service = str(prior_service['std'])
+                train.alternate_status = str(prior_service['etd'])
+            elif train.estimated_arrival != 'On time':
+                train.status = 'Delayed'
+            write_index = index
+            for items, values in service_times.items():
+                if isinstance(values, dict) and values['arrival_time'] == train.arrival_time:
+                    write_index = items
+            service_times[write_index] = train.__dict__
+    return service_times
 
 
 @app.route("/getstatus", methods=["GET"])
-def status_check():
-    global myurl
-    global mytimes
-    global origin
-    global dest
-    origin = request.args.get('origin')
-    dest = request.args.get('dest')
-    mytimes = request.args.get('mytimes')
-    myurl = f"http://huxley.apphb.com/all/{origin}/to/{dest}/{mytimes}"
-    output = checker()
-    return render_template('index.html', traindata=mytrains, trainstation=time_trains_location(), trainservices=time_trains_services())
+def main_page():
+    return render_template('test.html', traindata=url_parameters(), location_data=get_location(), trainservice=get_services())
 
 
 if __name__ == '__main__':
